c681e30dcefa787f766c4c897729a0f7482123b0
Pedro Osternack Correa <pedrost.rj@gmail.com>
2021-10-23 09:17:11 -0400

    1.2 adding Listing model

---
+++ src/RVEzy/RVEzy/Models/Listing.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RVEzy.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string ListingUrl { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public PropertyType PropertyType { get; set; }
+    }
+
+    public enum PropertyType
+    {
+        Apartment,
+        House
+    }
+}
